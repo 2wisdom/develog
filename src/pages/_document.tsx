9be43926b279c2cdc67291ba295b9f4558d3d422
Lizzie <leejihye7117@gmail.com>
2023-05-03 01:52:13 +0900

    favicon 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,7 +23,7 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
       <Head>
         {/* PWA primary color */}
         <meta name="theme-color" content={theme.palette.primary.main} />
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/tiger-face.png" />
         <meta name="emotion-insertion-point" content="" />
         {emotionStyleTags}
       </Head>
