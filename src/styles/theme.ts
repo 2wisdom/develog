651c2d8392e77a0505b2adf7f7bb5c2104f242be
Lizzie <leejihye7117@gmail.com>
2023-05-11 16:34:35 +0900

    MUI theme 설정 (indigo)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,6 @@
 import { Roboto } from "next/font/google";
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
+import { red, indigo } from "@mui/material/colors";
 
 export const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -13,10 +13,10 @@ export const roboto = Roboto({
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: indigo[500],
     },
     secondary: {
-      main: "#19857b",
+      main: indigo[400],
     },
     error: {
       main: red.A400,
