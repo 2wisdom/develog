ef30f62c378d4ef9802bf266f1d59e48e69c9ab9
Lizzie <leejihye7117@gmail.com>
2023-05-18 23:34:20 +0900

    개발 / 일상 path 구분

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -2,10 +2,15 @@ import fs from "fs";
 import path from "path";
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), "posts", "develop");
+export const POSTS_PATH_DEVELOP = path.join(process.cwd(), "posts", "develop");
+export const POSTS_PATH_DAILY = path.join(process.cwd(), "posts", "daily");
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
-export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
+export const postFilePathsDevelop = fs
+  .readdirSync(POSTS_PATH_DEVELOP)
   // Only include md(x) files
+  .filter((path) => /\.mdx?$/.test(path));
+
+export const postFilePathsDaily = fs
+  .readdirSync(POSTS_PATH_DAILY)
   .filter((path) => /\.mdx?$/.test(path));
