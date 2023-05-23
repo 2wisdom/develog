9bc803cc68ffc6022f901817f86dd16c5a62e6b3
Lizzie <leejihye7117@gmail.com>
2023-05-24 00:16:50 +0900

    폴더구조 수정

--- src/utils/mdxUtils.ts
+++ src/utils/mdxUtils.ts
@@ -2,8 +2,18 @@ import fs from "fs";
 import path from "path";
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH_DEVELOP = path.join(process.cwd(), "posts", "develop");
-export const POSTS_PATH_DAILY = path.join(process.cwd(), "posts", "daily");
+export const POSTS_PATH_DEVELOP = path.join(
+  process.cwd(),
+  "src",
+  "posts",
+  "develop"
+);
+export const POSTS_PATH_DAILY = path.join(
+  process.cwd(),
+  "src",
+  "posts",
+  "daily"
+);
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePathsDevelop = fs
